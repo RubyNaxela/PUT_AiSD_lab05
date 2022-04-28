e187f1bba16fb7bb67fcf2f21506fa8069085f2b
RubyNaxela <rubynaxela@gmail.com>
2022-04-29 00:43:30 +0200

    Implemented DFS sorting draft

--- graphlib/algorithms.hpp
+++ graphlib/algorithms.hpp
@@ -1,7 +1,11 @@
 #ifndef AISD_ALGORITHMS_HPP
 #define AISD_ALGORITHMS_HPP
+#pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
+#pragma ide diagnostic ignored "UnusedLocalVariable"
+#pragma ide diagnostic ignored "UnusedParameter"
 
 #include <map>
+#include <stack>
 #include <vector>
 #include <amgraph.hpp>
 #include <splgraph.hpp>
@@ -29,9 +33,53 @@ namespace gr {
     template<typename Gr>
     requires std::is_base_of_v<graph, Gr>
     std::vector<int> sort_dfs(const Gr& __graph) {
+
+        Gr graph = __graph;
         std::map<int, vertex_state> states;
+        std::stack<int> path;
+
+        auto white_successor = [&](int vertex) -> int {
+            for (int v: graph.successors(vertex)) if (states[v] == vertex_state::untouched) return v;
+            return -1;
+        };
+
+        auto any_white = [&]() -> bool {
+            for (int v: graph.all_vertices()) if (states[v] == vertex_state::untouched) return true;
+            return false;
+        };
+
+        for (int vertex: graph.all_vertices()) states[vertex] = vertex_state::untouched;
         std::vector<int> vertices;
 
+        step_4:
+        int u = graph.find_independent();
+        if (states[u] != vertex_state::untouched) {
+            u = *std::find_if(scan(graph.all_vertices()), [&](int v) { return states[v] == vertex_state::untouched; });
+        }
+
+        step_5:
+        path.push(u);
+        states[u] = vertex_state::touched;
+
+        step_6:
+        int u_successor = white_successor(u);
+        if (u_successor != -1) {
+            u = u_successor;
+            goto step_5;
+        } else {
+            states[u] = vertex_state::sorted;
+            vertices.insert(vertices.begin(), u);
+        }
+
+        path.pop();
+        if (not path.empty()) {
+            u = path.top();
+            goto step_6;
+        } else {
+            if (vertices.size() == graph.all_vertices().size()) goto step_11;
+            if (any_white()) goto step_4;
+        }
+        step_11:
         return vertices;
     }
 }
