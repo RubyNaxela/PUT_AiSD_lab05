8c2954a251b798aadc1e80bad270cc0940764669
RubyNaxela <rubynaxela@gmail.com>
2022-04-28 12:58:46 +0200

    Created the stateful_successors_list_dir_graph class

--- graphlib/splgraph.hpp
+++ graphlib/splgraph.hpp
@@ -7,6 +7,40 @@
 
 namespace gr {
 
+    struct stateful_vertex {
+        enum state {
+            untouched, touched, sorted
+        };
+        int value;
+        state state;
+    };
+
+    std::ostream& operator<<(std::ostream& out, const stateful_vertex& vertex) {
+        return out << vertex.value << "("
+                   << (vertex.state == 0 ? "untouched" : (vertex.state == 1 ? "touched" : "sorted")) << ")";
+    }
+
+    struct stateful_successors_list_dir_graph : graph<stateful_vertex> {
+
+        stateful_successors_list_dir_graph(const std::initializer_list<std::vector<stateful_vertex>>& __init)
+                : graph(__init) {}
+
+        explicit stateful_successors_list_dir_graph(const std::vector<std::vector<stateful_vertex>>& __init)
+                : graph(__init) {}
+
+        [[nodiscard]] bool connected(stateful_vertex vertex1, stateful_vertex vertex2) const override {
+            return false;
+        }
+
+        [[nodiscard]] std::vector<stateful_vertex> adjacent_nodes(stateful_vertex vertex) const override {
+            return {};
+        }
+
+        [[nodiscard]] std::vector<std::pair<stateful_vertex, stateful_vertex>> all_edges() const override {
+            return {};
+        }
+    };
+
     struct successors_list_dir_graph : int_graph {
 
         successors_list_dir_graph(const std::initializer_list<std::vector<int>>& __init) : int_graph(__init) {}
@@ -24,6 +58,16 @@ namespace gr {
                 }
             }
             return successors_list_dir_graph(list_vec);
+        }
+
+        [[nodiscard]] stateful_successors_list_dir_graph stateful() {
+            std::vector<std::vector<stateful_vertex>> vec;
+            for (const auto& row : *this) {
+                std::vector<stateful_vertex> new_row;
+                for (const auto& cell : row) new_row.push_back({cell, stateful_vertex::state::untouched});
+                vec.push_back(new_row);
+            }
+            return stateful_successors_list_dir_graph(vec);
         }
 
         [[nodiscard]] bool connected(int vertex1, int vertex2) const override {
