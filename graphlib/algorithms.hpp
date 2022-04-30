ba12a361532e27cea678272e3fa9b8487beacf10
RubyNaxela <rubynaxela@gmail.com>
2022-04-30 23:50:51 +0200

    Refactored the sort_dfs function

--- graphlib/algorithms.hpp
+++ graphlib/algorithms.hpp
@@ -13,10 +13,6 @@
 
 namespace gr {
 
-    enum vertex_state {
-        untouched, touched, sorted
-    };
-
     template<typename Gr>
     requires std::is_base_of_v<graph, Gr>
     std::vector<int> sort_bfs(const Gr& __graph) {
@@ -32,54 +28,82 @@ namespace gr {
 
     template<typename Gr>
     requires std::is_base_of_v<graph, Gr>
-    std::vector<int> sort_dfs(const Gr& __graph) {
+    class dfs_wrapper {
 
-        Gr graph = __graph;
+        enum vertex_state {
+            untouched, touched, sorted
+        };
+
+        const Gr& graph;
         std::map<int, vertex_state> states;
         std::stack<int> path;
 
-        auto white_successor = [&](int vertex) -> int {
-            for (int v : graph.successors(vertex)) if (states[v] == vertex_state::untouched) return v;
+    public:
+
+        explicit dfs_wrapper(const Gr& source) : graph(source) {
+            for (int vertex : graph.all_vertices()) states[vertex] = vertex_state::untouched;
+        }
+
+        [[nodiscard]] int find_white() const {
+            const int vertex = graph.find_independent();
+            if (states.at(vertex) == vertex_state::untouched) return vertex;
+            return *std::find_if(scan(graph.all_vertices()),
+                                 [&](int v) { return states.at(v) == vertex_state::untouched; });
+        };
+
+        void visit(int vertex) {
+            path.push(vertex);
+            states[vertex] = vertex_state::touched;
+        }
+
+        [[nodiscard]] int current_vertex() const {
+            return path.top();
+        }
+
+        [[nodiscard]] int white_successor() const {
+            for (int v : graph.successors(current_vertex())) if (states.at(v) == vertex_state::untouched) return v;
             return -1;
         };
 
-        auto any_white = [&]() -> bool {
-            for (int v : graph.all_vertices()) if (states[v] == vertex_state::untouched) return true;
-            return false;
+        [[nodiscard]] bool can_advance() const {
+            return white_successor() != -1;
+        }
+
+        void mark_sorted() {
+            states[current_vertex()] = vertex_state::sorted;
+        }
+
+        void go_back() {
+            path.pop();
+        }
+
+        [[nodiscard]] bool path_empty() const {
+            return path.empty();
+        }
+
+        [[nodiscard]] bool any_white() const {
+            const auto& vertices = graph.all_vertices();
+            return std::any_of(scan(vertices), [&](int v) { return states.at(v) == vertex_state::untouched; });
         };
+    };
 
-        for (int vertex : graph.all_vertices()) states[vertex] = vertex_state::untouched;
+    template<typename Gr>
+    requires std::is_base_of_v<graph, Gr>
+    std::vector<int> sort_dfs(const Gr& graph) {
+        dfs_wrapper<Gr> dfs_graph(graph);
         std::vector<int> vertices;
-
-        step_4:
-        int u = graph.find_independent();
-        if (states[u] != vertex_state::untouched) {
-            u = *std::find_if(scan(graph.all_vertices()), [&](int v) { return states[v] == vertex_state::untouched; });
-        }
-
-        step_5:
-        path.push(u);
-        states[u] = vertex_state::touched;
-
-        step_6:
-        int u_successor = white_successor(u);
-        if (u_successor != -1) {
-            u = u_successor;
-            goto step_5;
-        } else {
-            states[u] = vertex_state::sorted;
-            vertices.insert(vertices.begin(), u);
-        }
-
-        path.pop();
-        if (not path.empty()) {
-            u = path.top();
-            goto step_6;
-        } else {
-            if (vertices.size() == graph.all_vertices().size()) goto step_11;
-            if (any_white()) goto step_4;
-        }
-        step_11:
+        do {
+            dfs_graph.visit(dfs_graph.find_white());
+            do {
+                if (not dfs_graph.can_advance()) {
+                    dfs_graph.mark_sorted();
+                    vertices.insert(vertices.begin(), dfs_graph.current_vertex());
+                    dfs_graph.go_back();
+                    continue;
+                }
+                dfs_graph.visit(dfs_graph.white_successor());
+            } while (not dfs_graph.path_empty());
+        } while (dfs_graph.any_white());
         return vertices;
     }
 }
